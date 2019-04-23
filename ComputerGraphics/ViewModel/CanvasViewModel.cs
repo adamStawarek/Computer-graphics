5edbb1587b6e3d0926fe53db6d1f7550d6b0806f
adam stawarek <adam.stawarek@op.pl>
2019-04-23 23:49:47 +0200

    clear canvas

--- ComputerGraphics/ViewModel/CanvasViewModel.cs
+++ ComputerGraphics/ViewModel/CanvasViewModel.cs
@@ -13,16 +13,22 @@ namespace ImageEditor.ViewModel
 {
     public class CanvasViewModel : ViewModelBase
     {
-
+        #region private fields
+        private const int CanvasColor = byte.MaxValue;
         private WriteableBitmap _bitmap;
         private const int BitmapWidth = 1000;
         private const int BitmapHeight = 1000;
         private readonly byte[,,] _pixels = new byte[BitmapHeight, BitmapWidth, 4];
         private int _stride;
         private Point? _lastPoint;
+        #endregion
 
+        #region commands
         public RelayCommand<object> ClickCommand { get; }
+        public RelayCommand ClearCanvasCommand { get; set; }
+        #endregion
 
+        #region properties
         public WriteableBitmap Bitmap
         {
             get => _bitmap;
@@ -32,7 +38,6 @@ namespace ImageEditor.ViewModel
                 RaisePropertyChanged("Bitmap");
             }
         }
-
         public List<RasterGraphicViewModel> RasterGraphics { get; set; } = new List<RasterGraphicViewModel>
         {
             new RasterGraphicViewModel("Digital Differential Analyzer", true),
@@ -40,13 +45,15 @@ namespace ImageEditor.ViewModel
             new RasterGraphicViewModel("Pixel copying", false),
             new RasterGraphicViewModel("Antialiased Line Generation", false),
             new RasterGraphicViewModel("Antialiased Circle Generation", false)
-        };
+        }; 
+        #endregion
 
         public CanvasViewModel()
         {
             ClickCommand = new RelayCommand<object>(Click);
-            InitializeBitmap();
-        }
+            ClearCanvasCommand=new RelayCommand(ResetBitmap);
+            ResetBitmap();
+        }  
 
         private void Click(object obj)
         {
@@ -61,7 +68,6 @@ namespace ImageEditor.ViewModel
                
                 _lastPoint = null;
             }
-
             SetBitmap();
         }
 
@@ -71,17 +77,17 @@ namespace ImageEditor.ViewModel
             switch (rasterGraphic?.Type)
             {
                 case "Digital Differential Analyzer":
-                    DrawDdaLine(point, (Point) _lastPoint);
+                    DDALine(point, (Point) _lastPoint);
                     break;
-                case "Midpoint circle":
-
+                case "Antialiased Line Generation":
+                    WuLine(point, (Point)_lastPoint);
                     break;
                 default:
                     break;
             }
         }
 
-        private void DrawDdaLine(Point p1, Point p2)
+        private void DDALine(Point p1, Point p2)
         {
             double dy = p2.Y - p1.Y;
             double dx = p2.X - p1.X;
@@ -109,28 +115,71 @@ namespace ImageEditor.ViewModel
 
         }
 
+        private void WuLine(Point p1,Point p2)
+        {
+            //var steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            //if (steep)
+            //{
+            //    Swap(ref x0, ref y0);
+            //    Swap(ref x1, ref y1);
+            //}
+            //if (x0 > x1)
+            //{                
+            //    Swap(ref x0, ref x1);
+            //    Swap(ref y0, ref y1);
+            //}
+            if (p1.X > p2.X)
+            {
+                var tmpX = p1.X;
+                var tmpY = p1.Y;
+                p1.X = p2.X;
+                p1.Y = p2.Y;
+                p2.X = tmpX;
+                p2.Y = tmpY;
+            }
+
+            double dx = p2.X - p1.X;
+            double dy = p2.Y - p1.Y;
+            double gradient = dy / dx;
+            double y = p1.Y + gradient;
+            for (var x = p1.X + 1; x <= p2.X - 1; x++)
+            {
+                DrawPoint(new Point(x, (int)y), 1,(byte)((1 - (y - (int)y))*byte.MaxValue));
+                DrawPoint(new Point(x, (int)y + 1), 1 ,(byte)((y - (int)y)*byte.MaxValue));
+                y += gradient;
+            }
+        }
+
+        public void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
         private double Round(double y)
         {
             return y < 0 ? y - 0.5 : y + 0.5;
         }
 
-        private void DrawPoint(Point p, int offset)
+        private void DrawPoint(Point p, int offset=1,byte intensity=0)
         {
             for (int i = -offset; i < offset; i++)
             for (int j = -offset; j < offset; j++)
             for (int k = 0; k < 3; k++)
-                _pixels[(int) p.Y + j, (int) p.X + i, k] = 0;
+                _pixels[(int) p.Y + j, (int) p.X + i, k] = intensity;
         }
 
-        private void InitializeBitmap()
+        #region bitmap initalization & clear
+        private void ResetBitmap()
         {
+            _lastPoint = null;
             SetPixelArray();
             SetBitmap();
         }
 
         private void SetPixelArray()
         {
-            const int canvasColor = byte.MaxValue - 10;
             for (int row = 0; row < BitmapHeight; row++)
             {
                 for (int col = 0; col < BitmapWidth; col++)
@@ -138,7 +187,7 @@ namespace ImageEditor.ViewModel
                     for (int i = 0; i < 3; i++)
                     {
 
-                        _pixels[row, col, i] = canvasColor;
+                        _pixels[row, col, i] = CanvasColor;
                     }
 
                     _pixels[row, col, 3] = byte.MaxValue;
@@ -167,6 +216,7 @@ namespace ImageEditor.ViewModel
             _stride = 4 * ((BitmapWidth * tmp.Format.BitsPerPixel + 31) / 32);
             tmp.WritePixels(rect, pixels1d, _stride, 0);
             Bitmap = tmp;
-        }
+        } 
+        #endregion
     }
 }
